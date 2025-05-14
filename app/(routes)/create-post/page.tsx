e62adeb3fda126b61ca0ae3394e5e554efc91bff
LeightonGuang <leighton.guang@icloud.com>
2025-05-14 06:35:52 +0100

    Feat: New create post page

---
+++ app/(routes)/create-post/page.tsx
@@ -0,0 +1,11 @@
+import CreateAnnouncementForm from "@/components/CreateAnnouncementForm";
+
+const CreatePostPage = () => {
+  return (
+    <section className="flex h-[calc(100dvh-36px)] w-full items-center justify-center">
+      <CreateAnnouncementForm />
+    </section>
+  );
+};
+
+export default CreatePostPage;
